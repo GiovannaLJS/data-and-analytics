bc21e01f76bed156a9b141a198916d4959c2928f
GiovannaLJS <gioh.lins@outlook.com>
2024-10-26 11:02:05 -0300

    tranformar arquivo de mp4 para mp3

---
+++ LabAutoation/mp3-mp4.py
@@ -0,0 +1,13 @@
+from moviepy.editor import *
+import uuid
+
+def mp4_to_mp3(mp4_filename, mp3_filename):
+    arquivo_a_ser_convertido = AudioFileClip(mp4_filename)
+    arquivo_a_ser_convertido.write_audiofile(mp3_filename)
+    arquivo_a_ser_convertido.close()
+
+if __name__ == "__main__":
+    mp4_filename = 'C:/Users/giohl/OneDrive/Área de Trabalho/Git/data-and-analytics/LabAutoation/entrevista de Boechat com Jô Soares 360.mp4'
+    mp3_filename = '{nome_arquivo}.mp3'.format(nome_arquivo = uuid.uuid4().hex)
+    
+    mp4_to_mp3(mp4_filename, mp3_filename)
